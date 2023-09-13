1f03b27d314bc885ac856b2cc3c2930e02de5613
Shubham Kumar <67545566+Shubham-2001-2243@users.noreply.github.com>
2023-09-13 11:45:57 +0530

    add Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+public class Multiply {
+    
+    
+        public static void main(String[] args)
+        {
+             int a=5,b=8;
+           
+                System.out.println(a*b);
+            
+        }
+    
+     
+    
+}
